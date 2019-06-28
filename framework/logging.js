250efe9c3dea470c9da0e1bae716ee24cc4da17d
llamasking <21324788+llamasking@users.noreply.github.com>
2019-06-28 14:06:15 -0700

    Add descriptions to framework

--- framework/logging.js
+++ framework/logging.js
@@ -1,3 +1,8 @@
+/*
+// Framework: Logging
+// Description: Logs given data with 'console.log', attaching the timestamp. 
+*/
+
 module.exports = (data) => {
     var time = new Date()
     console.log(`${time.toDateString()} ${time.toLocaleTimeString()} -- ${data}`)
