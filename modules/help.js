663011bda5449c489b7d078ce75ce5bbe2238afe
llamasking <21324788+llamasking@users.noreply.github.com>
2019-06-28 14:07:03 -0700

    Minor changes to help command

--- modules/help.js
+++ modules/help.js
@@ -8,8 +8,8 @@ const Discord = require("discord.js");
 module.exports = (message, args) => {
     var embed = new Discord.RichEmbed()
         .setTitle("Commands")
-        .addField("!!help, !!commands", "This command.")
-        .addField("!!amalgamate [User1] [User2]...", "Creates an amalgamtion of two or more people's tweets using Markov chains.")
+        .addField("!!help / commands", "This command.")
+        .addField("!!ama / amalgamate [User1] [User2]...", "Creates an amalgamtion of two or more people's tweets using Markov chains.")
         .addField("!!credits", "Thanks so much, Wildcard0!")
         .addField("!!ping", "How shitty is my DSL internet today?")
         .setImage("https://raw.githubusercontent.com/llamasking/badboi/master/assets/rainbow.gif")
