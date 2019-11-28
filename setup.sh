e675f2d5d20a95ef1c8a2662eaa6f43a7a3f238a
llamasking <21324788+llamasking@users.noreply.github.com>
2019-11-27 22:43:56 -0800

    Add reference to setup.py from twitblend

--- setup.sh
+++
@@ -1,6 +0,0 @@
-python3 -m venv ./virt/
-source ./virt/bin/activate
-cd ./Twitblend/
-pip install -e .
-
-echo "Installed! run 'source ./virt/bin/activate' to use Twitblend. In the venv, it acts as a regular app."
