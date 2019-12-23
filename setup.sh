2d94c483d4423a74b1839e9f3d4c2ee80b7199e7
llamasking <21324788+llamasking@users.noreply.github.com>
2019-12-22 23:59:03 -0800

    Try 2

--- setup.sh
+++
@@ -1,6 +0,0 @@
-python3 -m venv ./venv/
-source ./venv/bin/activate
-cd ./Twitblend/
-pip install -e .
-
-echo "Installed! run 'source ./venv/bin/activate' to enter Twitblend's venv. In the venv, it acts as a regular application and can be called with 'twitblend'."
