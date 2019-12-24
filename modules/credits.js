ef78b8074eaa39323b0f0d2a8a2d1b154252eda1
llamasking <21324788+llamasking@users.noreply.github.com>
2019-12-23 17:32:32 -0800

    Make Codacity happy again.

--- modules/credits.js
+++ modules/credits.js
@@ -10,6 +10,6 @@ module.exports = (message) => {
         .addField("→ Wildcard0: https://github.com/wildcard0/", "Created Twitblend, the program used to amalgamate tweets. ")
         .addField("→ llamasking: https://github.com/llamasking/", "Creator and host of both the Discord and Twitter bots.")
         .setColor(0x7289DA)
-        .setFooter("If you ever run into issues, feel free to join the support server. https://discord.gg/zFxjHYp")
+        .setFooter("If you ever run into issues, feel free to join the support server. https://discord.gg/zFxjHYp");
     message.channel.send({ embed });
-}
+};
--- modules/help.js
+++ modules/help.js
@@ -12,8 +12,8 @@ module.exports = (message) => {
         .addField("!!ama / amalgamate / blend / twitblend [User1] [User2]...", "Creates an amalgamtion of two or more people's tweets using Markov chains.")
         .addField("!!credits", "Thanks so much, Wildcard0!")
         .addField("!!ping", "How much mor amazing is Heroku's internet than mine?")
-        .setImage("https://raw.githubusercontent.com/llamasking/badboi/master/assets/rainbow.gif")
+        .setImage("https://raw.githubusercontent.com/llamasking/TwitblendBot-Discord/master/rainbow.gif")
         .setColor(0x7289DA)
-        .setFooter("If you ever run into issues, feel free to join the support server. https://discord.gg/zFxjHYp")
+        .setFooter("If you ever run into issues, feel free to join the support server. https://discord.gg/zFxjHYp");
     message.channel.send({ embed });
-}
+};
